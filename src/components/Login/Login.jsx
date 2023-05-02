d1aebcbeae62fea792d74d18512b104bbf64f938
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-05-02 20:35:19 +0600

    Authenticate Login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
+
 
 const Login = () => {
+
+  
+
+    const { signInWithGoogle, signIn } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+    console.log('login page location', location)
+    const from = location.state?.from?.pathname || '/'
+
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+
+        signIn(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                navigate(from, { replace: true })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle().then(() => {
+            console.log('User signed in with Google');
+            navigate('/', { replace: true }); 
+        }).catch(error => {
+            console.log('Error signing in with Google:', error);
+        });
+    }
     return (
         <div>
             <div className="hero min-h-screen ">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    
+
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="form-control mt-6">
-                                <button className="btn btn-outline my-3 text-black ">Google</button>
-                                <button className="btn btn-outline text-black  ">GitHub</button>
-                            </div>
-                        <form >
+                        <div className="form-control mt-6">
+                        <button onClick={handleGoogleSignIn} className="btn btn-outline my-3 text-black ">Google</button>
+                           
+                        </div>
+                        <form onSubmit={handleLogin} >
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -25,7 +65,7 @@ const Login = () => {
                                 </label>
                                 <input type="password" name='password' placeholder="Password" className="input input-bordered" required />
                                 <label className="label">
-                                <p> Don't Have an Account? <Link to="/register" className='link text-red-500 hover:text-red-800'>Register</Link></p>
+                                    <p> Don't Have an Account? <Link to="/register" className='link text-red-500 hover:text-red-800'>Register</Link></p>
 
                                 </label>
                             </div>
--- src/components/providers/AuthProvider.jsx
+++ src/components/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import { GithubAuthProvider, GoogleAuthProvider,  TwitterAuthProvider,  createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import {  GoogleAuthProvider,  createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 import { useEffect } from 'react';
 import { useState } from 'react';
@@ -9,6 +9,8 @@ export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
+
+
 const googleProvider = new GoogleAuthProvider({
     scopes: [
         'https://www.googleapis.com/auth/userinfo.email',
@@ -16,9 +18,9 @@ const googleProvider = new GoogleAuthProvider({
     ]
 });
 
-const githubProvider = new GithubAuthProvider();
 
-const twitterProvider = new TwitterAuthProvider();
+
+
 
 
 const AuthProvider = ({ children }) => {
@@ -40,22 +42,16 @@ const AuthProvider = ({ children }) => {
         return signOut(auth);
     }
 
+   
+
     const signInWithGoogle = () => {
         setLoading(true);
         return signInWithPopup(auth, googleProvider);
     }
 
-    const signInWithGithub = () => {
-        setLoading(true);
-        return signInWithPopup(auth, githubProvider);
-    }
-    
   
-    const signInWithTwitter = () => {
-        setLoading(true);
-        return signInWithPopup(auth, twitterProvider);
-      }
-
+  
+   
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, loggedUser => {
@@ -76,9 +72,7 @@ const AuthProvider = ({ children }) => {
         signIn,
         logOut,
         signInWithGoogle,
-        signInWithGithub,
-        signInWithTwitter,
-      
+        
     }
 
     return (
